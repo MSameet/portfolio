398ac778d8584bf33eda1ad5c517e48dcd4794a4
Muhammad Sameet Ul Hassan <89504995+MSameet@users.noreply.github.com>
2024-08-12 15:13:22 +0500

    portfolio changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwzWJdX2IVICpM_vukPDnALHOR6PlycrQ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
   authDomain: "volunteer-management-sys-bde65.firebaseapp.com",
   projectId: "volunteer-management-sys-bde65",
   storageBucket: "volunteer-management-sys-bde65.appspot.com",
   messagingSenderId: "758037748934",
-  appId: "1:758037748934:web:b356d1a5f8692de6798bc7"
+  appId: "1:758037748934:web:b356d1a5f8692de6798bc7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 export { db };
